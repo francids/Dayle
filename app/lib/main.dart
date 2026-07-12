d5a9ef1e1ab045af8c46b1dd225578b263e5f55d
Francisco Mesa <francisco.mc.ids@gmail.com>
2026-07-12 19:05:19 -0400

    refactor: remove entire mission feature set and reset main app screen

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,31 +1,18 @@
 import "package:flutter/material.dart";
 
-import "screens/mission_screen.dart";
-import "services/database_service.dart";
-import "services/mission_repository.dart";
-import "services/mission_service.dart";
-import "theme/app_theme.dart";
-
 void main() {
-  final repository = MissionRepository(DayleDatabase(), MissionService());
-
-  runApp(DayleApp(repository: repository));
+  runApp(const DayleApp());
 }
 
 class DayleApp extends StatelessWidget {
-  final MissionRepository repository;
-
-  const DayleApp({super.key, required this.repository});
+  const DayleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Dayle",
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      themeMode: ThemeMode.system,
-      home: MissionScreen(repository: repository),
+      home: Scaffold(body: Center(child: Text("Dayle"))),
     );
   }
 }
